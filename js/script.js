88d70bc376f4394c76f6e4eb0355ba03ae7670d0
Rahul <rahultimbaliya555@gmail.com>
2023-07-23 17:41:12 +0530

    22/07/2023

--- js/script.js
+++ js/script.js
@@ -85,8 +85,8 @@ window.onscroll = () => {
 };
 
 const sr = ScrollReveal ({
-	distance: '45px',
-	duration: 2700,
+	distance: '40px',
+	duration: 2000,
 	reset: true
 })
 
@@ -94,7 +94,3 @@ sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
-
-
-
-
