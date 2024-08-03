debaaff6a6b8ccd1dcca98b1263cdea7d1a11172
Rahul Timbaliya <rahultimbaliya555@gmail.com>
2024-08-03 00:00:28 -0700

    Update Date:03/08/2024

--- js/script.js
+++ js/script.js
@@ -1,74 +1,72 @@
 const header = document.querySelector("header");
 
 
-var about=document.getElementById("myabout")
-about.addEventListener("click",function(){
-	var a =new Audio("sound/Tbne9iqUf64ba924cac96b1d5d02605041f2f052atKMGmOqPT.mp3")
+var about = document.getElementById("myabout")
+about.addEventListener("click", function () {
+	var a = new Audio("sound/Tbne9iqUf64ba924cac96b1d5d02605041f2f052atKMGmOqPT.mp3")
 	a.play()
 })
 
-var project=document.getElementById("myproject")
-project.addEventListener("click",function(){
-	var a =new Audio("sound/1683565933220yb9hqs0v-voicemaker.in-speech.mp3")
+var project = document.getElementById("myproject")
+project.addEventListener("click", function () {
+	var a = new Audio("sound/1683565933220yb9hqs0v-voicemaker.in-speech.mp3")
 	a.play()
 })
 
-var contact=document.getElementById("mycontact")
-contact.addEventListener("click",function(){
-	var a =new Audio("sound/contact.mp3")
+var contact = document.getElementById("mycontact")
+contact.addEventListener("click", function () {
+	var a = new Audio("sound/contact.mp3")
 	a.play()
 })
-var cv=document.getElementById("cv")
-cv.addEventListener("click",function(){
-	var a =new Audio("sound/cv.mp3")
-	a.play()
-})
-
-
-var certificate=document.getElementById("certificate")
-certificate.addEventListener("click",function(){
-	var a =new Audio("sound/certificate.mp3")
-	a.play()
-})
-
-var backtohome=document.getElementById("back")
-backtohome.addEventListener("mouseenter",function(){
-	var a =new Audio("sound/backtohome.mp3")
-	a.play()
-})
-
-var fimiliar=document.getElementById("fimiliar")
-fimiliar.addEventListener("click",function(){
-	var a =new Audio("sound/fimiliar.mp3")
+var cv = document.getElementById("cv")
+cv.addEventListener("click", function () {
+	var a = new Audio("sound/cv.mp3")
 	a.play()
 })
 
 
-var github=document.getElementById("github")
-github.addEventListener("mouseenter",function(){
+var certificate = document.getElementById("certificate")
+certificate.addEventListener("click", function () {
+	var a = new Audio("sound/certificate.mp3")
+	a.play()
+})
 
-	
-	var a =new Audio("sound/github.mp3")
+var backtohome = document.getElementById("back")
+backtohome.addEventListener("mouseenter", function () {
+	var a = new Audio("sound/backtohome.mp3")
+	a.play()
+})
+
+var fimiliar = document.getElementById("fimiliar")
+fimiliar.addEventListener("click", function () {
+	var a = new Audio("sound/fimiliar.mp3")
+	a.play()
+})
+
+
+var github = document.getElementById("github")
+github.addEventListener("mouseenter", function () {
+	var a = new Audio("sound/github.mp3")
 	a.play()
 })
 
 
 
-  var t=100
-  setInterval(function(){
-	if(t==45){
-	   t=70
-	   document.getElementById("top").style.marginTop="-100px"
-    }
-	else{
-		document.getElementById("top").style.marginTop="0px"
-		t=45
+var t = 100;
+setInterval(function () {
+	if (t == 45) {
+		t = 70
+		document.getElementById("top").style.marginTop = "-100px"
 	}
-	document.getElementById("top").style.height=t+"px"
-  },1000)
+	else {
+		document.getElementById("top").style.marginTop = "0px"
+		t = 45
+	}
+	document.getElementById("top").style.height = t + "px"
+}, 1000)
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+	header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 let menu = document.querySelector('#menu-icon');
@@ -84,13 +82,13 @@ window.onscroll = () => {
 	navlist.classList.remove('active');
 };
 
-const sr = ScrollReveal ({
+const sr = ScrollReveal({
 	distance: '40px',
 	duration: 2000,
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal('.home-text', { delay: 350, origin: 'left' })
+sr.reveal('.home-img', { delay: 350, origin: 'right' })
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', { delay: 200, origin: 'bottom' })
